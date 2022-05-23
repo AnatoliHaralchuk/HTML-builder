dd4363a7fec7bc85d154b43b74b0c504b2a4dbf8
AnatoliHaralchuk <sshades@mail.ru>
2022-05-23 23:37:19 +0300

    feat: 4 and 5 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -11,7 +11,6 @@ fs.readdir(path.join(__dirname, 'secret-folder'),
         if (file.isFile()){
             fs.stat(path.join(__dirname,'secret-folder',file.name), (err, stats) => {
                 if (err) throw err
-            
             console.log(`${path.parse(file.name).name} - ${path.extname(file.name).slice(1)} - ${stats.size/1000}kb`);
         })
         }
